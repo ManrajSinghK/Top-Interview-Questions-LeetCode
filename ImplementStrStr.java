de5e953d5860290553b015759d3a9b55da4c5faf
ManrajSinghK <79503532+ManrajSinghK@users.noreply.github.com>
2021-03-25 05:13:33 +0530

    Create ImplementStrStr.java

---
+++ ImplementStrStr.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int strStr(String haystack, String needle) 
+    {
+        if(haystack.length() == 0 || needle.length() == 0 )
+            return 0;
+        
+        for(int i=0;i< haystack.length() - needle.length() +1;i++)
+        {
+            int j;
+            for(j=0;j<needle.length();j++)
+            {
+                if(haystack.charAt(i+j) != needle.charAt(j))
+                    break;
+            }
+            if(j==needle.length())
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
