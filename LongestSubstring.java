2f3d5605620c2bbd84260a924779dc2c63038b60
ManrajSinghK <79503532+ManrajSinghK@users.noreply.github.com>
2021-03-25 16:48:01 +0530

    Create LongestSubstring.java

---
+++ LongestSubstring.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) 
+    {
+        HashSet<Character> set= new HashSet();
+        
+        int max=0;
+        int a_pointer=0;
+        int b_pointer=0;
+        
+        while(b_pointer<s.length())
+        {
+            if(!set.contains(s.charAt(b_pointer)))
+            {
+                set.add(s.charAt(b_pointer));
+                b_pointer++;
+                max=Math.max(set.size(),max);
+            }
+            else
+            {
+                set.remove(s.charAt(a_pointer));
+                a_pointer++;
+            }
+        }
+        return max;
+        
+    }
+}
