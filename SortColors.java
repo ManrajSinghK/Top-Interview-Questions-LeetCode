4a967d1566fb4fd18de4c02e2a91c98f3183f34d
Manraj Singh <79503532+ManrajSinghK@users.noreply.github.com>
2021-04-13 20:36:43 +0530

    Create SortColors.java

---
+++ SortColors.java
@@ -0,0 +1,35 @@
+class Solution {
+    public void sortColors(int[] nums) 
+    {
+        if(nums.length == 0)
+            return;
+        
+        int end=nums.length - 1;
+        int start=0;
+        int index=0;
+        
+        while(start<=end && index<=end)
+        {
+            if(nums[index] == 0)
+            {
+                nums[index]=nums[start];
+                nums[start]=0;
+                
+                index++;
+                start++;
+            }
+            else if(nums[index] == 2)
+            {
+                nums[index] = nums[end];
+                nums[end]=2;
+                
+                end--;
+            }
+            else
+            {
+                index++;
+            }
+        }
+        
+    }
+}
